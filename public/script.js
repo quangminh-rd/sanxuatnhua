4cb0ca4ce3bb74bea26495923e1fe72c360e7cfc
ducanh297 <ducanh-rd@quangminhpro.com>
2024-12-24 15:16:16 +0700

    u11

--- public/script.js
+++ public/script.js
@@ -439,7 +439,7 @@ async function findRowInSheet(maLenhsanxuatURI) {
                 document.getElementById('tenHatnhua4').textContent = orderDetails.tenHatnhua4; // Thêm phần này
                 document.getElementById('socanHatnhua4').textContent = formatWithCommas(orderDetails.socanHatnhua4);
 
-                document.getElementById('tongSocanHatnhua').textContent = formatWithCommas(orderDetails.tongSocanHatnhua);
+                document.getElementById('tongSocanHatnhua').textContent = orderDetails.tongSocanHatnhua;
 
                 document.getElementById('maThanhphamPB1').textContent = orderDetails.maThanhphamPB1;
                 document.getElementById('socanHN1_maThanhphamPB1').textContent = formatWithCommas(orderDetails.socanHN1_maThanhphamPB1);
